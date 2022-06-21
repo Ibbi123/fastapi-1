4af562dc598f0f28c0fd5952c23d7adc7752ebd4
Ibrahim Mirza <ibbi@Ibrahims-MacBook-Air.local>
2022-06-21 14:28:57 +0300

    Added API Authentication

--- main.py
+++ main.py
@@ -1,19 +1,47 @@
-from fastapi import FastAPI
+# from fastapi import Depends, FastAPI
+# from fastapi.security import OAuth2PasswordBearer
+# from fastapi import FastAPI
+from fastapi import FastAPI, Body, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+
+api_keys = [
+    "akljnv13bvi2vfo0b0bw"
+]  # This is encrypted in the database
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")  # use token authentication
+
+# headers = {
+#   'Content-Type': 'application/json',
+#   'Authorization': 'akljnv13bvi2vfo0b0bw'
+# }
+
+def api_key_auth(api_key: str = Depends(oauth2_scheme)):
+    if api_key not in api_keys:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Forbidden"
+        )
 
 app = FastAPI()
-                 
+
+
 fake_db = []                   
+
+@app.get("/items/",)
+async def read_items(token: str = Depends(api_key_auth,)):
+    return {"token": token}
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
-
+ 
 @app.get("/items/{item_id}")
 def get_item(item_id: int):
     return {"item_id": item_id}
 
 @app.post("/items/")
 def create_item(item: dict):
+
     fake_db.append(item)
     return item
 
